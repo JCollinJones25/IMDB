3c3ddada26b26be6bc0fbb70b69505ffa8a6c832
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-04-26 14:06:34 -0400

    index and show routes made on movie controller

---
+++ controllers/movies_controllers.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// index route
+router.get('/', async (req, res, next) => {
+    try {
+        const movies = await db.Movie.find({})
+        const context = {movies}
+        return res.render('index.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+// new route
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+// show route
+router.get('/:id/', async (req, res, next) => {
+    try {
+        const foundMovie = await db.Movie.findById(req.params.id)
+        const allActors = await db.Actor.find({movie: req.params.id})
+        const context = {
+            oneMovie: foundMovie,
+            actors: allActors
+        }
+        return res.render('show.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
