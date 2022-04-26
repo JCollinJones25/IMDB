dcfe4a7099ca95d806e869359df14d2aac362b74
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-04-26 14:27:20 -0400

    delete and update - movie controller finished

--- controllers/movies_controllers.js
+++ controllers/movies_controllers.js
@@ -7,6 +7,7 @@ router.get('/', async (req, res, next) => {
     try {
         const movies = await db.Movie.find({})
         const context = {movies}
+        console.log(movies)
         return res.render('index.ejs', context)
     } catch (error) {
         console.log(error);
@@ -25,6 +26,7 @@ router.get('/:id/', async (req, res, next) => {
     try {
         const foundMovie = await db.Movie.findById(req.params.id)
         const allActors = await db.Actor.find({movie: req.params.id})
+        console.log(allActors.length, "Actors Found")
         const context = {
             oneMovie: foundMovie,
             actors: allActors
@@ -41,6 +43,7 @@ router.get('/:id/', async (req, res, next) => {
 router.get('/:id/edit', async (req, res, next) => {
     try {
         const updatedMovie = await db.Movie.findById(req.params.id)
+        console.log(updatedMovie)
         const context = {
             movie: updatedMovie
         }
@@ -51,3 +54,45 @@ router.get('/:id/edit', async (req, res, next) => {
         return next();
     }
 })
+
+// create route
+router.post('/', async (req, res, next) => {
+    try {
+        const createdMovie = await db.Movie.create(req.body)
+        console.log(`Created Movie: ${createdMovie}`)
+        res.redirect('/movies')
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+// delete route
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deletedMovie = await db.Movie.findByIdAndDelete(req.params.id)
+        const deletedActors = await db.Actor.deleteMany({movie: req.params.id})
+        console.log(deletedActors)
+        return res.redirect('/movies')
+    } catch {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+// update route 
+router.put('/:id', async (req, res, next) => {
+    try {
+        const updatedMovie = await db.Movie.findByIdAndUpdate(req.params.id, req.body)
+        console.log(updatedMovie)
+        return res.redirect('/movies')
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+module.exports = router
