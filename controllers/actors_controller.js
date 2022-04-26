b1fe2395caff8b0503382b5a2c891efb2591d134
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-04-26 14:46:44 -0400

    index and new routes for actors controller

--- controllers/actors_controller.js
+++ controllers/actors_controller.js
@@ -3,7 +3,29 @@ const router = express.Router()
 const db = require('../models')
 
 // index route
+router.get('/', async (req, res, next) => {
+    try {
+        const allActors = await db.Actor.find({})
+        res.send(allActors)
+    } catch (erro) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
+// new route 
+router.get('/new', async (req, res, next) => {
+    try {
+        const allMovies = await db.Movie.find({})
+        const context = { movies: allMovies }
+        res.render('actors/new.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
 
 
